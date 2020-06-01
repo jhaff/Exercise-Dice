5dc9fe46e521ec6d86ca2796a2dfec244bac49bc
Jacob Haff <jacobhaff2@gmail.com>
2020-05-31 20:18:10 -0400

    Initial Commit

---
+++ Exercise Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise Dice
+//
+//  Created by Jacob Haff on 5/31/20.
+//  Copyright © 2020 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
